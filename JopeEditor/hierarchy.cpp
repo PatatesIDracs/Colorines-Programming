3ca9fc94566cd6384ddc815eb308ee675686b2da
Pere Rifà Planellas <pere.rifaplanellas@gmail.com>
2019-03-27 21:11:07 +0100

    Save and Load Scenes
    TODO: GameObjects Save and Load

--- JopeEditor/hierarchy.cpp
+++ JopeEditor/hierarchy.cpp
@@ -3,6 +3,8 @@
 #include "inspector.h"
 #include "gameobject.h"
 #include <iostream>
+#include <QFileDialog>
+#include <QDir>
 #include "QPainter"
 
 Hierarchy::Hierarchy(QWidget *parent) :
@@ -129,7 +131,54 @@ void Hierarchy::OpenScene()
 {
     std::cout << "Open Scene" << std::endl;
 
+    qDeleteAll(objects.begin(), objects.end());
+    objects.clear();
+
+    // Clear listWidget items
+    ui->listWidget->clear();
+
+    selected = nullptr;
+
     // Open file widget
+    QString file_name = QFileDialog::getOpenFileName(this, "Open a file", QDir::homePath());
+
+    QFile f(file_name);
+
+    if(!f.open(QFile::ReadOnly))
+    {
+        return;
+    }
+
+    QDataStream stream(&f);
+    stream.setVersion(QDataStream::Qt_4_0);
+
+    QString text;
+    int numObj;
+    stream >> text;
+    stream >> numObj;
+
+    if(numObj > 0){
+        objects.reserve(numObj);
+        uint i = 0;
+        for(; i < numObj; i++)
+        {
+            GameObject* temp = new GameObject(i);
+            stream >> temp->name;
+
+            objects.push_back(temp);
+            ui->listWidget->addItem(temp->name);
+        }
+
+        ui->listWidget->scrollToBottom();
+        ui->listWidget->currentRowChanged(i);
+        ui->listWidget->setCurrentRow(i);
+
+        selected = objects[i-1];
+        emit SigHierarchyUpdate(selected);
+        inspector->SetSelectedGO(selected);
+    }
+
+    f.close();
 
     emit SigHierarchyUpdate(nullptr);
 }
@@ -139,7 +188,30 @@ void Hierarchy::SaveScene()
     std::cout << "Save Scene" << std::endl;
 
     // SaveScene
+    QString file_name = QFileDialog::getSaveFileName(this, "Save file", "");
 
+    if(!file_name.endsWith(".jope"))
+        file_name += ".jope";
+
+    QFile f(file_name);
+
+    if(!f.open(QFile::WriteOnly))
+    {
+        return;
+    }
+
+    QDataStream outstream(&f);
+    outstream << QString("Scene Gameobjects");
+    outstream << objects.size();
+
+    outstream.setVersion(QDataStream::Qt_4_0);
+
+    for(uint i = 0; i < objects.size(); i++)
+    {
+        outstream << objects[i]->name;
+    }
+
+    f.close();
 }
 
 void Hierarchy::UndoAction()
